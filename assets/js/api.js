7e4633a244e3f660b21f792c4f0191d77b37376b
Titanio112 <aphmgbr@gmail.com>
2023-05-08 18:58:07 -0300

    Ajustando as informações de perfil

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,2 +1,8 @@
 
 
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/Titanio112/Portfolio/master/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
+
